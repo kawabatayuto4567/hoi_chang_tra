09509d7a1ef40cf139c3760ff3c232b1b9b4f2d2
kawabatayuto4567 <kawabatayuto4567@github.com>
2025-05-09 10:21:11 +0700

    init

--- b-i-l-i-b-i-l-i-m-a-n-g-a.meta.js
+++ b-i-l-i-b-i-l-i-m-a-n-g-a.meta.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         b-i-l-i-b-i-l-i-m-a-n-g-a
 // @namespace    b-i-l-i-b-i-l-i-m-a-n-g-a.user.js
-// @version      2025.03.27
+// @version      2025.05.09
 // @description  down b-i-l-i-b-i-l-i-m-a-n-g-a
 // @author       You
 // @include      /^https:\/\/m[a4]ng[a4]\.b[i1]lib[i1]l[i1]\.c[o0]m\/mc.+?\/.+?/
 // @icon         https://raw.githubusercontent.com/kawabatayuto4567/hoi_chang_tra/main/b-i-l-i-b-i-l-i-m-a-n-g-a.png
 // @grant        none
-// @require      https://cdn.jsdelivr.net/npm/@zip.js/zip.js@2.7.53/dist/zip.min.js
+// @require      https://cdn.jsdelivr.net/npm/@zip.js/zip.js@2.7.60/dist/zip.min.js
 // @downloadURL  https://github.com/kawabatayuto4567/hoi_chang_tra/raw/main/b-i-l-i-b-i-l-i-m-a-n-g-a.user.js
 // @updateURL    https://github.com/kawabatayuto4567/hoi_chang_tra/raw/main/b-i-l-i-b-i-l-i-m-a-n-g-a.meta.js
 // ==/UserScript==
